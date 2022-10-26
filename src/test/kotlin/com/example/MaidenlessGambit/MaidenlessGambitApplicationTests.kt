d423b2f84233741a17f3fda49120ceada3bb71f7
Shivam Badal <55099336+shivam-badal@users.noreply.github.com>
2022-10-26 22:59:25 +0200

    init

---
+++ src/test/kotlin/com/example/MaidenlessGambit/MaidenlessGambitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.MaidenlessGambit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaidenlessGambitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
